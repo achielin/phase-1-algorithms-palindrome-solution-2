42f31ad78d2d5ef8c016fe2a37730019724bd6e3
linder Opondo <achielinder@gmail.com>
2023-10-06 15:39:51 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let startIndex = 0;
+  let endIndex = word.length - 1;
+
+  while (startIndex < endIndex) {
+    if (word[startIndex] != word[endIndex]) {
+      return false;
+    }
+    startIndex++;
+    endIndex--;
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
 */
-
 /*
   Add written explanation of your solution here
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
